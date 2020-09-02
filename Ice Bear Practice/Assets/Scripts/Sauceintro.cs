3fbec1464ed39cc28897ffaa70fc22065f7402ea
kepaesler <kepaesler@gmail.com>
2020-09-02 13:49:31 -0700

    Slight level tweaks

---
+++ Ice Bear Practice/Assets/Scripts/Sauceintro.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sauceintro : MonoBehaviour
+{
+    private Collider2D m_Collider;
+
+    [SerializeField]
+    private Dialogue dial;
+
+    [SerializeField]
+    private string dialog;
+    private float transitionTime = 2.5f;
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            dial.changeDialogue(dialog, transitionTime);
+            Destroy(this.gameObject);
+        }
+    }
+}
