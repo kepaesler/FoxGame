81781bd91b881253fefe8f0b9fa75651ce28fa02
kepaesler <kepaesler@gmail.com>
2020-09-06 21:09:18 -0700

    Added eagle movement to start screen

--- Ice Bear Practice/Assets/Scripts/Enemy/EagleStartScreen.cs
+++ Ice Bear Practice/Assets/Scripts/Enemy/EagleStartScreen.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class EagleStartScreen : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField]
+    private Vector3 velocity;
+
+    private Vector3 pos1;
+    private Vector3 pos2;
+    private Vector3 pos3;
+
+    [SerializeField]
+    private Vector3 posDiff;
+
+    [SerializeField]
+    private Vector3 posDiff2;
+
+
     void Start()
     {
-        
+        pos1 = transform.position;
+        pos2 = pos1 + posDiff;
+        pos3 = pos1 + posDiff2;
+        //Flip();
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
-        
+        if (Vector3.Distance(transform.position, pos2) <= 25)
+        {
+            //transform.position = pos1;
+            velocity = new Vector3(velocity.x, -velocity.y, 0);
+        }
+        else if (Vector3.Distance(transform.position, pos3) <= 25)
+        {
+            velocity = new Vector3(-velocity.x, -velocity.y, 0);
+            Flip();
+        }
+        else if ((Vector3.Distance(transform.position, pos1) <= .01f))
+        {
+            velocity = new Vector3(-velocity.x, -velocity.y, 0);
+            Flip();
+        }
+        transform.position += (velocity * Time.deltaTime);
+    }
+
+    private void Flip()
+    {
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
     }
 }
