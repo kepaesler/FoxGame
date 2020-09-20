821e9a6f28fd92ece6dc32db0197827444716f32
kepaesler <kepaesler@gmail.com>
2020-09-20 16:59:26 -0700

    Changed camera settings

---
+++ Ice Bear Practice/Assets/Scripts/BackgroundMove/Background2.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background2 : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 move;
+    void Update()
+    {
+        // background matches the player's position
+        this.transform.position = player.transform.position + move;
+    }
+}
